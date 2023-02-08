dbaeeaf4acdaf07181a54b4199ae7f2ea924157a
polzovatel <nursejitsadirov@gmail.com>
2023-02-09 03:03:15 +0600

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,26 @@
+public class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    void method() {
+        System.out.println(name + " This is number one car!!!");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Mercedes mercedes = new Mercedes("Mersedes","S class",500000);
+        System.out.println("~~~~~~~~~~~~~~~~~~~");
+        mercedes.method();
+        System.out.println("~~~~~~~~~~~~~~~~~~~");
+        System.out.println(mercedes.string());
+        System.out.println("~~~~~~~~~~~~~~~~~~~");
+        System.out.println(mercedes);
+        System.out.println("~~~~~~~~~~~~~~~~~~~");
+    }
+}
